01deb86c740b31f1691e0f596519b54a50cc2f0e
Mathy <vanhoefm@users.noreply.github.com>
2015-01-15 15:43:26 -0500

    build.sh: option to prevent installation of FW images

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 make -C target_firmware
-if [ "$?" -eq 0 ]; then
+if [ "$?" -eq 0 -a "$#" -le 0 ]; then
 	echo -e "\nInstalling firmware images, this requires root privileges."
 	sudo cp ./target_firmware/htc_7010.fw /lib/firmware/htc_7010.fw
 	sudo cp ./target_firmware/htc_9271.fw /lib/firmware/htc_9271.fw
