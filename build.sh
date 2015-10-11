5829959894544b1deb24ee8e201c5de6dff26342
Mathy <vanhoefm@users.noreply.github.com>
2015-10-11 21:01:56 +0200

    Ask to install firmware images

--- build.sh
+++ build.sh
@@ -3,7 +3,16 @@
 make -C target_firmware
 tar -cf ../firmware.tar target_firmware/htc_*.fw
 if [ "$?" -eq 0 -a "$#" -le 0 ]; then
-	echo -e "\nInstalling firmware images, this requires root privileges."
-	sudo cp ./target_firmware/htc_7010.fw /lib/firmware/htc_7010.fw
-	sudo cp ./target_firmware/htc_9271.fw /lib/firmware/htc_9271.fw
+
+	echo ""
+	read -r -p "Install firmware images? This requires root privileges. [y/N] " response
+	case $response in
+		[yY][eE][sS]|[yY]) 
+			sudo cp ./target_firmware/htc_7010.fw /lib/firmware/htc_7010.fw
+		        sudo cp ./target_firmware/htc_9271.fw /lib/firmware/htc_9271.fw
+			;;
+		*)
+			echo "Skipping installation..."
+			;;
+	esac
 fi
